0d19076207c88521e8bc4e9fca0868517c958970
Alex-HP <aleksandr.lozinskiy2005@gmail.com>
2023-10-07 22:30:26 -0600

    app was created

---
+++ src/components/app-info/app-info.jsx
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Employee accounting at Google</h1>
+            <h2>Amount of employees: </h2>
+            <h2>The prize will be given to: </h2>
+        </div>
+    );
+};
+
+export default AppInfo;
---
+++ src/components/search-panel/search-panel.jsx
@@ -0,0 +1,9 @@
+import './search-panel.css'
+
+const SearchPanel = () => {
+    return (
+        <input placeholder="Find an employee" className="form-control search-input" type="text" />
+    );
+};
+
+export default SearchPanel;
